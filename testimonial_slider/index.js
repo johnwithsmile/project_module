b261949fe2495cd996498a0efaa4d2b5cb9c18ed
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2023-05-25 23:06:33 +0900

    testimonial_slider

---
+++ testimonial_slider/index.js
@@ -0,0 +1,42 @@
+const testimonials = [
+  {
+    name: "Lorianna X",
+    photoUrl:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
+    text: " Buy this now. It's exactly what I've been looking for. J's Apple saved my business.",
+  },
+  {
+    name: "Eve S",
+    photoUrl:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    text: "Definitely worth the investment. J's Apple has got everything I need.",
+  },
+  {
+    name: "Carly N",
+    photoUrl:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
+    text: "J's Apple is awesome! Thanks guys, keep up the good work!",
+  },
+];
+
+const imgEl = document.querySelector("img");
+const textEl = document.querySelector(".text");
+const usernameEl = document.querySelector(".username");
+
+let idx = 0;
+
+updateTestimonial();
+
+function updateTestimonial() {
+  const { name, photoUrl, text } = testimonials[idx];
+  imgEl.src = photoUrl;
+  textEl.innerText = text;
+  usernameEl.innerText = name;
+  idx++;
+  if (idx === testimonials.length) {
+    idx = 0;
+  }
+  setTimeout(() => {
+    updateTestimonial();
+  }, 2000);
+}
